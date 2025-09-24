c15aba52b1ec20b593db66a1ae5672c391de9028
Ola van Dunen <vandunen@envisioning.solutions>
2025-09-24 15:26:24 +0200

    initial desktop-app

---
+++ src/main/java/module-info.java
@@ -0,0 +1,13 @@
+module com.gfu.desktop {
+    requires javafx.controls;
+    requires javafx.fxml;
+
+    requires org.controlsfx.controls;
+    requires com.dlsc.formsfx;
+    requires net.synedra.validatorfx;
+    requires org.kordamp.ikonli.javafx;
+    requires org.kordamp.bootstrapfx.core;
+
+    opens com.gfu.desktop to javafx.fxml;
+    exports com.gfu.desktop;
+}
