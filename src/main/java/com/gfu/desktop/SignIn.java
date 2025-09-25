fc7d60f65b075a306f269e914a9f8e90f6295fe4
Ola van Dunen <vandunen@envisioning.solutions>
2025-09-25 13:50:22 +0200

    bug fix

--- src/main/java/com/gfu/desktop/SignIn.java
+++ src/main/java/com/gfu/desktop/SignIn.java
@@ -10,38 +10,56 @@ import login.LoginInterface;
 public class SignIn {
 
 
+    static {
+        // Disable GlassFish SSL before any initialization
+        System.setProperty("com.sun.enterprise.iiop.ssl.disable", "true");
+        System.setProperty("com.sun.enterprise.security.httpsOutboundKeyAlias", "");
+        System.setProperty("javax.net.ssl.keyStore", "");
+        System.setProperty("javax.net.ssl.trustStore", "");
+    }
+
+
+
     private AddressInterface addressInterface;
     private LoginInterface loginInterface;
 
-    public SignIn() {
-        try {
-            lookupEJB();
-        } catch (NamingException e) {
-            throw new RuntimeException(e);
-        }
-    }
-    private void lookupEJB() throws NamingException {
-        // JNDI Properties für GlassFish
-        Properties props = new Properties();
-        props.setProperty(Context.INITIAL_CONTEXT_FACTORY,
-                "com.sun.enterprise.naming.SerialInitContextFactory");
-        props.setProperty(Context.URL_PKG_PREFIXES,
-                "com.sun.enterprise.naming");
-        props.setProperty(Context.PROVIDER_URL,
-                "iiop://localhost:3700");  // GlassFish IIOP Port
 
-        // InitialContext erstellen
-        final Context context = new InitialContext(props);
+
+
+
+    public SignIn() {
+
 
         // JNDI Name für Ihr EJB - Format:
         // java:global/[EAR-Name]/[EJB-JAR-Name]/[Bean-Class-Name]![Interface-Name]
         final String jndiNameAdress = "java:global/ear-module-1.0.0/ejb-module/Address!address.AddressInterface";
         final String jndiNameLogin = "java:global/ear-module-1.0.0/ejb-module/Login!login.LoginInterface";
-
-        // Remote EJB lookup
-        addressInterface = (AddressInterface) context.lookup(jndiNameAdress);
-        loginInterface = (LoginInterface) context.lookup(jndiNameLogin);
+        addressInterface = lookupEJB(jndiNameAdress, AddressInterface.class);
     }
+
+
+
+    public static InitialContext createContext() throws NamingException {
+        Properties props = new Properties();
+
+        // Use standard CORBA naming - no GlassFish specifics
+        props.put(Context.INITIAL_CONTEXT_FACTORY,
+                "com.sun.jndi.cosnaming.CNCtxFactory");
+        props.put(Context.PROVIDER_URL, "iiop://localhost:3700");
+
+        return new InitialContext(props);
+    }
+
+    public static <T> T lookupEJB(String jndiName, Class<T> interfaceClass) {
+        try {
+            InitialContext ctx = createContext();
+            Object obj = ctx.lookup(jndiName);
+            return interfaceClass.cast(obj);
+        } catch (NamingException e) {
+            throw new RuntimeException("EJB lookup failed for: " + jndiName, e);
+        }
+    }
+
 
     public  AddressInterface getAddressInterface() {
         return addressInterface;
